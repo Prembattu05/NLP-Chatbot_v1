777cfcd6c075087ddb494006f2bd4bd63ad2d9bd
Prembattu05 <battuprem05@gmail.com>
2025-07-25 20:14:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.title("💬 Smart NLP Chatbot with Fuzzy Matching")
 @st.cache_data
 def load_data():
     try:
-        df = pd.read_excel("chatbot_dataset_augmented.csv")
+        df = pd.read_excel("chatbot_dataset.txt")
         df = df.dropna()
         return df
     except Exception as e:
